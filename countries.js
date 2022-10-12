32c99c0694b6f4a6fc7aecc67aab880ceb980bd5
mudjaycker <tombutoyi@gmail.com>
2022-10-12 17:31:24 +0200

    Revert "using right name of The Democratic Republic of the Congo  instead of  "Congo, The Democratic Republic of the""
    This reverts commit 24234deecd60b72478ed741d6301078542f5d391.

--- countries.js
+++ countries.js
@@ -49,7 +49,7 @@ const countries = [
     {name: 'Colombia', code: 'CO'}, 
     {name: 'Comoros', code: 'KM'}, 
     {name: 'Congo', code: 'CG'}, 
-    {name: 'The Democratic Republic of the Congo', code: 'CD'}, 
+    {name: 'Congo, The Democratic Republic of the', code: 'CD'}, 
     {name: 'Cook Islands', code: 'CK'}, 
     {name: 'Costa Rica', code: 'CR'}, 
     {name: 'Cote D\'Ivoire', code: 'CI'}, 
